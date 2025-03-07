096d8f926bc3de508537015d86163b7126be444f
MojtabaNabid <mojtaba.nabid@gmail.com>
2025-03-07 15:41:16 +0100

    optimizing if, else conditions

--- app.js
+++ app.js
@@ -2,56 +2,65 @@
 
 let choose = "";
 let computerChoice = "";
-let userScore = 0;
-let computerScore = 0;
+let userScore = 1;
+let computerScore = 1;
 
 while (computerScore < 3 || userScore < 3) {
+  console.log("Start of the program");
   choose = prompt("Rock | Paper | Scissor,  Which one?");
+  console.log(choose);
   if (
     choose !== "Rock" &&
     choose !== "Paper" &&
     choose !== "Scissor" &&
+    choose === null &&
     choose === ""
   ) {
     alert("Please choose Rock, Paper or Scissor (It is case Sensitive).");
-    location.reload();
+    // location.reload();
   } else {
     computerChoice = computerByChoice();
     console.log("You choosed: ", choose);
     console.log("Computer choosed: ", computerChoice);
     if (computerChoice === choose) {
-      console.log("Both are the same, No Score!!");
+      console.log("It's a tie.");
       console.log("=======================================");
-    } else if (computerChoice === "Rock" && choose === "Scissor") {
-      computerScore += 1;
-      console.log("computer scores: ", computerScore);
-      console.log("Your scores: ", userScore);
-      console.log("=======================================");
-    } else if (computerChoice === "Rock" && choose === "Paper") {
-      userScore += 1;
-      console.log("computer scores: ", computerScore);
-      console.log("Your scores: ", userScore);
-      console.log("=======================================");
-    } else if (computerChoice === "Paper" && choose === "Scissor") {
-      userScore += 1;
-      console.log("computer scores: ", computerScore);
-      console.log("Your scores: ", userScore);
-      console.log("=======================================");
-    } else if (computerChoice === "Paper" && choose === "Rock") {
-      computerScore += 1;
-      console.log("computer scores: ", computerScore);
-      console.log("Your scores: ", userScore);
-      console.log("=======================================");
-    } else if (computerChoice === "Scissor" && choose === "Rock") {
-      userScore += 1;
-      console.log("computer scores: ", computerScore);
-      console.log("Your scores: ", userScore);
-      console.log("=======================================");
-    } else if (computerChoice === "Scissor" && choose === "Paper") {
-      computerScore += 1;
-      console.log("computer scores: ", computerScore);
-      console.log("Your scores: ", userScore);
-      console.log("=======================================");
+    } else if (computerChoice === "Rock") {
+      if (choose === "Scissor") {
+        computerScore += 1;
+        console.log("computer scores: ", computerScore);
+        console.log("Your scores: ", userScore);
+        console.log("=======================================");
+      } else if (choose === "Paper") {
+        userScore += 1;
+        console.log("computer scores: ", computerScore);
+        console.log("Your scores: ", userScore);
+        console.log("=======================================");
+      }
+    } else if (computerChoice === "Scissor") {
+      if (choose === "Paper") {
+        computerScore += 1;
+        console.log("computer scores: ", computerScore);
+        console.log("Your scores: ", userScore);
+        console.log("=======================================");
+      } else if (choose === "Rock") {
+        userScore += 1;
+        console.log("computer scores: ", computerScore);
+        console.log("Your scores: ", userScore);
+        console.log("=======================================");
+      }
+    } else {
+      if (choose === "Rock") {
+        computerScore += 1;
+        console.log("computer scores: ", computerScore);
+        console.log("Your scores: ", userScore);
+        console.log("=======================================");
+      } else if (choose === "Scissor") {
+        userScore += 1;
+        console.log("computer scores: ", computerScore);
+        console.log("Your scores: ", userScore);
+        console.log("=======================================");
+      }
     }
   }
 }
@@ -66,11 +75,10 @@ if (userScore > computerScore) {
   console.log("Computer won the match, maybe next time!!!");
 }
 
-
 function computerByChoice() {
-    const choices = ["Rock", "Paper", "Scissor"];
-    let randomNumebr = Math.floor(Math.random() * choices.length);
-    console.log(randomNumebr);
-    let computer = choices[randomNumebr];
-    return computer;
-  }
+  const choices = ["Rock", "Paper", "Scissor"];
+  let randomNumebr = Math.floor(Math.random() * choices.length);
+  console.log("Random Number is: ", randomNumebr);
+  let computer = choices[randomNumebr];
+  return computer;
+}
