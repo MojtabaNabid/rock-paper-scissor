60d95137ede6fa98884379c5653e8854c8c681f8
MojtabaNabid <mojtaba.nabid@gmail.com>
2025-03-10 13:53:26 +0100

    prompt method is not recommanded to use - last commit

--- app.js
+++ app.js
@@ -1,58 +1,54 @@
 // Rock | Paper | Scrissor
-
-let userChoice = "";
-let computerChoice = "";
+const choices = ["rock", "paper", "scissor"];
 let userScore = 0;
 let computerScore = 0;
 let result = [];
 
-const checkWinner = (player, computer) => {
-  if (
-    player !== "Rock" &&
-    player !== "Paper" &&
-    player !== "Scissor" &&
-    player === null &&
-    player === ""
-  ) {
-    alert("Please choose Rock, Paper or Scissor (It is case Sensitive).");
-    // location.reload();
-  } else {
-    console.log("You choosed: ", player);
-    console.log("Computer choosed: ", computer);
-    if (computer === player) {
-      console.log("It's a tie.");
-      console.log("=======================================");
-    } else if (computer === "Rock") {
-      player === "Scissor" ? (computerScore += 1) : (userScore += 1);
-    } else if (computer === "Scissor") {
-      player === "Paper" ? (computerScore += 1) : (userScore += 1);
-    } else {
-      player === "Rock" ? (computerScore += 1) : (userScore += 1);
-    }
-  }
-  let result = [userScore, computerScore];
-  return result;
-};
-
-userChoice = prompt("Rock | Paper | Scissor,  Which one?");
-// console.log(userChoice);
-computerChoice = computerByChoice();
-result = checkWinner(userChoice, computerChoice);
-
-// console.log("============================");
-console.log("Your total scores:", result[0]);
-console.log("Computer's total scores:", result[1]);
-
-if (result[0] > result[1]) {
-  console.log("Congratulations! You won the match!!!");
-} else if (result[1] > result[0]) {
-  console.log("Computer won the match, maybe next time!!!");
-}
-
 function computerByChoice() {
-  const choices = ["Rock", "Paper", "Scissor"];
   let randomNumebr = Math.floor(Math.random() * choices.length);
   //   console.log("Random Number is: ", randomNumebr);
   let computer = choices[randomNumebr];
   return computer;
 }
+
+const checkWinner = (player, computer) => {
+  if (choices.indexOf(player) !== -1) {
+    console.log("You choosed: ", player);
+    console.log("Computer choosed: ", computer);
+    if (computer === player) {
+      console.log("It's a tie.");
+    } else if (computer === "rock") {
+      player === "scissor" ? (computerScore += 1) : (userScore += 1);
+    } else if (computer === "scissor") {
+      player === "paper" ? (computerScore += 1) : (userScore += 1);
+    } else {
+      player === "rock" ? (computerScore += 1) : (userScore += 1);
+    }
+  } else {
+    console.log("You cheated!!");
+    // return undefined;
+    // userScore = 0;
+    // computerScore = 0;
+  }
+};
+
+const play = () => {
+  let player = prompt("rock | paper | scissor,  Which one?");
+  // console.log(player);
+  let computer = computerByChoice();
+  // console.log(computer);
+  checkWinner(player, computer);
+
+  console.log("Your score: ", userScore);
+  console.log("Computer's score: ", computerScore);
+  console.log("........................");
+  if (userScore >= 3 || computerScore >= 3) {
+    let result =
+      userScore >= computerScore ? "Congrats! You won." : "Sorry, You lost!";
+    console.log(result);
+  } else {
+    play();
+  }
+};
+
+play();
